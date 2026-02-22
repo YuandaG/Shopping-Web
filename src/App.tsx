d4f71ed8ba8d464b8acfe4ac2e68cb82521c47c2
Yuanda Gao <yuanda.gao94@gmail.com>
2026-02-22 17:43:46 +0000

    Fix: use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
 import { Recipes } from './pages/Recipes';
@@ -7,7 +7,7 @@ import { Settings } from './pages/Settings';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen pb-20">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
         </Routes>
         <Navigation />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
